40ebce128cd11e77fe9e3a69bf9fc31533a79e67
Paolo D'Aquino <pablo.duck997@gmail.com>
2021-05-03 23:25:02 +0200

    add environment configuration scriptable object
    add fallen door prefab
    add meterials

---
+++ Assets/Scripts/DungeonGeneretionScripts/EnvConfiguration.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Environment Data Config", menuName = "Environment Data Config")]
+[System.Serializable]
+public class EnvConfiguration : ScriptableObject
+{
+    [SerializeField] private Material _groundMaterial;
+    [SerializeField] private Material _wallsMaterial;
+    [SerializeField] private Material _malusMaterial;
+
+
+    public Material GroundMaterial { get => _groundMaterial; }
+    public Material WallsMaterial { get => _wallsMaterial; }
+    public Material MalusMaterial { get => _malusMaterial; }
+
+
+}
