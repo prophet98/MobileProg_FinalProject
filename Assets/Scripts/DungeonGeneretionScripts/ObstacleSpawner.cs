ac24339cf5fceadf38c24fd72cf0d3ee3b29ef30
Paolo D'Aquino <pablo.duck997@gmail.com>
2021-05-10 09:40:42 +0200

    update obstacles spawner

--- Assets/Scripts/DungeonGeneretionScripts/ObstacleSpawner.cs
+++ Assets/Scripts/DungeonGeneretionScripts/ObstacleSpawner.cs
@@ -4,27 +4,51 @@ using UnityEngine;
 
 public class ObstacleSpawner : MonoBehaviour
 {
-    //TODO: Implement
-    private GameObject[] obstaclesSpwanPoints;
+    private List<GameObject> obstacleSpawnPoints;
+    private List<GameObject> prefabsObstacleSpawns;
     private EnvConfiguration envConfig;
 
     private void Start()
     {
+        prefabsObstacleSpawns = new List<GameObject>();
+        obstacleSpawnPoints = new List<GameObject>();
+        Refresh();
     }
     private void OnEnable()
     {
         Door.OnEnvChange += Refresh;
     }
 
+    private void OnDisable()
+    {
+        Door.OnEnvChange += Refresh;
+    }
+
     private void Refresh()
     {
+        DestroyOldObstacles();
+
         envConfig = GetComponent<RandomizeConfig>().GetCurrentConfig;
+        Debug.Log(envConfig.name);
 
-        obstaclesSpwanPoints = GameObject.FindGameObjectsWithTag("Obstacle");
+        obstacleSpawnPoints.AddRange(GameObject.FindGameObjectsWithTag("Obstacle"));
 
-        for(int i = 0; i < obstaclesSpwanPoints.Length; i++)
+        for (int i = 0; i < GameObject.FindGameObjectsWithTag("Obstacle").Length; i++)
         {
-            Instantiate(envConfig.OstacleGO, obstaclesSpwanPoints[i].transform);
+            prefabsObstacleSpawns.Add(Instantiate(envConfig.OstacleGO, obstacleSpawnPoints[i].transform.position, Quaternion.identity));
         }
+        Debug.Log(obstacleSpawnPoints.Count.ToString());
+    }
+
+    private void DestroyOldObstacles()
+    {
+        for (int i = 0; i < prefabsObstacleSpawns.Count; i++)
+        {
+            Destroy(prefabsObstacleSpawns[i], 0f);
+            Debug.Log("Destroying");
+        }
+
+        obstacleSpawnPoints.Clear();
+        prefabsObstacleSpawns.Clear();
     }
 }
