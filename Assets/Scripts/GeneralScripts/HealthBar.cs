c42d065d3917a1f3ad29a67f5d2b7ac65991a6b3
Alessandro Sciarra <ale.sciarra@gmail.com>
2021-05-21 11:24:24 +0200

    Added player health bar on screen

--- Assets/Scripts/GeneralScripts/HealthBar.cs
+++ Assets/Scripts/GeneralScripts/HealthBar.cs
@@ -1,4 +1,5 @@
 
+using System;
 using System.Collections;
 using DamageScripts;
 using UnityEngine;
@@ -8,10 +9,12 @@ public class HealthBar : MonoBehaviour
 {
     [SerializeField] private Image foregroundImage;
     [SerializeField] private float updateSpeedSeconds = 0.5f;
+    [SerializeField] private HealthComponent healthComponent;
+    [SerializeField] private bool IsPlayerBar;
     private void Awake()
     {
         foregroundImage = GetComponentInChildren<Image>();
-        GetComponentInParent<HealthComponent>().OnHealthPctChange += HandleHealthChange;
+        healthComponent.OnHealthPctChange += HandleHealthChange;
     }
 
     private void HandleHealthChange(float pct)
@@ -31,13 +34,26 @@ public class HealthBar : MonoBehaviour
         {
             elapsed += Time.deltaTime;
             foregroundImage.fillAmount = Mathf.Lerp(preChangePct, pct, elapsed / updateSpeedSeconds);
+            if (IsPlayerBar)
+            {
+                foregroundImage.color = Color.red;
+            }
             yield return null;
         }
-        
+        foregroundImage.color = Color.cyan;
         foregroundImage.fillAmount = pct;
     }
     private void LateUpdate()
     {
+        if (IsPlayerBar)
+        {
+            return;
+        }
         transform.LookAt(Camera.main.transform);
+    }
+
+    private void OnDisable()
+    {
+        healthComponent.OnHealthPctChange += HandleHealthChange;
     }
 }
