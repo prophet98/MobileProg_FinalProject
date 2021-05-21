adc00cda8d97765f1cdf4a4ecc969daec07f7af5
Paolo D'Aquino <pablo.duck997@gmail.com>
2021-05-21 10:00:49 +0200

    modify obstacles spawner logic

--- Assets/Scripts/DungeonGeneretionScripts/ObstacleSpawner.cs
+++ Assets/Scripts/DungeonGeneretionScripts/ObstacleSpawner.cs
@@ -4,51 +4,20 @@ using UnityEngine;
 
 public class ObstacleSpawner : MonoBehaviour
 {
-    private List<GameObject> obstacleSpawnPoints;
-    private List<GameObject> prefabsObstacleSpawns;
+    private GameObject obstacleInstance;
     private EnvConfiguration envConfig;
 
-    private void Start()
-    {
-        prefabsObstacleSpawns = new List<GameObject>();
-        obstacleSpawnPoints = new List<GameObject>();
-        Refresh();
-    }
     private void OnEnable()
     {
-        Door.OnEnvChange += Refresh;
+        envConfig = GameObject.FindObjectOfType<RandomizeConfig>().GetCurrentConfig;
+        SpawnObstacle();
     }
-
     private void OnDisable()
     {
-        Door.OnEnvChange += Refresh;
+        Destroy(obstacleInstance);
     }
-
-    private void Refresh()
+    private void SpawnObstacle()
     {
-        DestroyOldObstacles();
-
-        envConfig = GetComponent<RandomizeConfig>().GetCurrentConfig;
-        Debug.Log(envConfig.name);
-
-        obstacleSpawnPoints.AddRange(GameObject.FindGameObjectsWithTag("Obstacle"));
-
-        for (int i = 0; i < GameObject.FindGameObjectsWithTag("Obstacle").Length; i++)
-        {
-            prefabsObstacleSpawns.Add(Instantiate(envConfig.OstacleGO, obstacleSpawnPoints[i].transform.position, Quaternion.identity));
-        }
-        Debug.Log(obstacleSpawnPoints.Count.ToString());
-    }
-
-    private void DestroyOldObstacles()
-    {
-        for (int i = 0; i < prefabsObstacleSpawns.Count; i++)
-        {
-            Destroy(prefabsObstacleSpawns[i], 0f);
-            Debug.Log("Destroying");
-        }
-
-        obstacleSpawnPoints.Clear();
-        prefabsObstacleSpawns.Clear();
+        obstacleInstance = Instantiate(envConfig.OstacleGO, transform.position, transform.rotation);
     }
 }
