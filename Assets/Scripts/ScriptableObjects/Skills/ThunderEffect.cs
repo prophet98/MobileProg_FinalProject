5167c6973d48e0e9ddc80f9b6ae954130a9cf717
Paolo D'Aquino <pablo.duck997@gmail.com>
2021-05-23 15:01:54 +0200

    Change thunder skill logic
    TODO: fix null reference

--- Assets/Scripts/ScriptableObjects/Skills/ThunderEffect.cs
+++ Assets/Scripts/ScriptableObjects/Skills/ThunderEffect.cs
@@ -6,29 +6,55 @@ using DamageScripts;
 public class ThunderEffect : MonoBehaviour
 {
     private const string ENEMY_TAG = "Enemy";
-    private List<GameObject> enemies;
+    private List<GameObject> enemiesList;
     [SerializeField]
-    private int damage = 3;
+    private int damage = 15;
+
+    private void Start()
+    {
+        enemiesList = new List<GameObject>();
+    }
 
     private void Damage(GameObject enemy)
     {
         enemy.GetComponentInParent<HealthComponent>().RemoveHealth(damage);
     }
-    private void OnTriggerStay(Collider other) //TODO: change
-    {
-       if (other.transform.parent.CompareTag(ENEMY_TAG))
-          Damage(other.gameObject);
-    }
 
+    private void EnemiesParse()
+    {
+        for (int i = 0; i < enemiesList.Count; i++)
+        {
+            if (enemiesList[i] != null) 
+            { 
+                Damage(enemiesList[i]);
+            }
+            else
+            {
+                enemiesList.Remove(enemiesList[i]);
+            }
+        }
+    }
+   
     private void OnTriggerEnter(Collider other)
     {
         if (other.transform.parent.CompareTag(ENEMY_TAG))
-            enemies.Add(other.gameObject);
+        {
+            if (!enemiesList.Contains(other.gameObject))
+            {
+                enemiesList.Add(other.gameObject);
+                InvokeRepeating(nameof(EnemiesParse), 0f, 0.5f);
+            }
+        }
     }
 
     private void OnTriggerExit(Collider other)
     {
         if (other.transform.parent.CompareTag(ENEMY_TAG))
-            enemies.Remove(other.gameObject);
+        {
+            if (enemiesList.Contains(other.gameObject))
+            {
+                enemiesList.Remove(other.gameObject);
+            }
+        }
     }
 }
