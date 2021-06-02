3c1f464305bb9330637d69dfc173eca9e4904079
Paolo D'Aquino <pablo.duck997@gmail.com>
2021-06-02 16:07:16 +0200

    Add score in dungeon scene

--- Assets/Scripts/PlayerScripts/BattleRewardSystem.cs
+++ Assets/Scripts/PlayerScripts/BattleRewardSystem.cs
@@ -1,16 +1,25 @@
 
 using System;
+using TMPro;
 using UnityEngine;
 
 public class BattleRewardSystem : MonoBehaviour
 {
     private int CurrentMoney { get; set; }
+    private float Score { get; set; }
     public bool canPassGate;
     public event Action UnlockDoors;
+
+    [SerializeField]
+    private TextMeshProUGUI scoreText;
+    [SerializeField]
+    private float scoreMultiplier = 2f;
 
     public void RewardPlayer(int money)
     {
         CurrentMoney += money;
+        Score = CurrentMoney * scoreMultiplier;
+        scoreText.text = "SCORE: " + Score.ToString();
         UnlockDoors?.Invoke();
     }
 
